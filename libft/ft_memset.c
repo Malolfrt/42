ba672a6a276bb946cdab14180b6dbedf95a2eb3d
Malo Lefort <mlefort@paul-f3Ar4s10.clusters.42paris.fr>
2023-11-07 18:01:52 +0100

    fin 07/11/2023

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -6,13 +6,13 @@
 /*   By: mlefort <mlefort@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 15:00:52 by mlefort           #+#    #+#             */
-/*   Updated: 2023/11/07 16:27:37 by mlefort          ###   ########.fr       */
+/*   Updated: 2023/11/07 17:58:43 by mlefort          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_memset(void *s, int c, size_t n)
+void	*ft_memset(void *s, int c, size_t n)
 {
 	size_t	i;
 	char	*str;
@@ -21,9 +21,24 @@ void	ft_memset(void *s, int c, size_t n)
 	str = s;
 	while (i <= n)
 	{
-		str = c;
+		str[i] = (unsigned char)c;
 		i++;
 	}
 	s = str;
-	return (s)
+	return (s);
+}
+
+int	main(void)
+{
+	void	*test;
+	void	*true;
+	char	*restest;
+	char	*restrue;
+	char	*tab[5];
+
+	test = ft_memset(tab, '1', 5);
+	true = memset(tab, '1', 5);
+	restest = test;
+	restrue = true;
+	printf("%s : %s", restest, restrue);
 }
